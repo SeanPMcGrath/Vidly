ee8ae71d1111cc0b698b8753c7a1adb9c82a2e89
Sean McGrath <spmcgrath9@gmail.com>
2019-04-07 16:02:06 -0700

    finished genres mongoose update

--- routes/genres.js
+++ routes/genres.js
@@ -1,6 +1,6 @@
-// const express = require("express");
-// const Joi = require("joi");
-// const router = express.Router();
+const express = require("express");
+const Joi = require("joi");
+const router = express.Router();
 
 //Run mongod in terminal
 //THEN CLOSE TERMINAL!!
@@ -8,115 +8,71 @@
 
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch(err => console.error("Could not connect to MongoDB...", err));
-
-const genreSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 3
-  }
-});
-
-const Genre = mongoose.model("Genre", genreSchema);
-
-async function createGenre(name) {
-  const genre = new Genre({
-    name: name
-  });
-
-  try {
-    await genre.validate();
-    const result = await genre.save();
-    console.log(result);
-  } catch (ex) {
-    for (field in ex.errors) console.log(ex.errors[field].message);
-  }
-}
-
-async function getGenres() {
-  const genres = await Genre.find();
-  console.log(genres);
-}
-
-async function updateGenre(name, newName) {
-  try {
-    const genre = await Genre.findOne({ name: name });
-    if (!genre) return;
-
-    genre.name = newName;
-
-    const result = await genre.save();
-    console.log(result);
-  } catch (ex) {
-    for (field in ex.errors) console.log(ex.errors[field].message);
-  }
-}
-
-async function removeGenre(name) {
-  try {
-    const result = await Genre.deleteOne({ name: name });
-    console.log(result);
-  } catch (ex) {
-    for (field in ex.errors) console.log(ex.errors[field].message);
-  }
-}
-
-createGenre();
-
-/*
-const genres = [
-  { name: "action" },
-  { name: "documentary" },
-  { name: "animated" },
-  { name: "comedy" }
-];
+const Genre = mongoose.model(
+  "Genre",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 3
+    }
+  })
+);
 
 //get genres object
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const genres = await Genre.find();
   res.send(genres);
 });
 
 //create new genre
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = {
-    name: req.body.name
-  };
-  genres.push(genre);
+  let genre = new Genre({ name: req.body.name });
+  genre = await genre.save();
   res.send(genre);
 });
 
 //edit genre
-router.put("/:name", (req, res) => {
-  const genre = genres.find(g => g.name === req.params.name);
-  if (!genre)
-    return res
-      .status(404)
-      .send("The genre with the given name could not be found");
-
+router.put("/:id", async (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  genre.name = req.body.name;
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    { new: true }
+  );
+
+  if (!genre)
+    return res
+      .status(404)
+      .send("The genre with the given id could not be found");
+
   res.send(genre);
 });
 
 //delete genre
-router.delete("/:name", (req, res) => {
-  const genre = genres.find(g => g.name === req.params.name);
+router.delete("/:id", async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+
   if (!genre)
     return res
       .status(404)
-      .send("The genre with the given name could not be found");
+      .send("The genre with the given id could not be found");
 
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
+  res.send(genre);
+});
+
+router.get("/:id", async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+
+  if (!genre)
+    return res
+      .status(404)
+      .send("The genre with the given id could not be found");
 
   res.send(genre);
 });
@@ -130,5 +86,3 @@ function validateGenre(genre) {
 }
 
 module.exports = router;
-
-*/
